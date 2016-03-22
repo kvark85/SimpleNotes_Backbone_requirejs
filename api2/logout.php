64efc2a55130a2cac19b39a8b6cf3d72dd40f869
kvark85 <kvark1985@gmail.com>
2016-03-23 01:09:01 +0200

    Исправлено нажатие на ссылку выхода.

--- api2/logout.php
+++ api2/logout.php
@@ -7,7 +7,7 @@ $sn_user_id = isset($_SESSION['sn_user_id']) ? $_SESSION['sn_user_id'] : "";
 if ( $sn_user_id != "" ) {
     setcookie('sn_user_id', '', time() - 3600);
     unset($_SESSION['sn_user_id']);
-
-    header('Location: http://localhost');
 }
+
+header('Location: http://localhost');
 ?>
