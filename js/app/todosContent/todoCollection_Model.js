1e37de069e711752c1730a7a2518d665d387e00a
kvark85 <kvark1985@gmail.com>
2016-03-26 22:44:34 +0200

    Удаление неиспользуемых файлов и замена favicon.

--- js/app/todosContent/todoCollection_Model.js
+++
@@ -1,7 +0,0 @@
-define(['backbone', 'app/todoItem/todo_Model'], function(Backbone, rTodoModel){
-    var Collection = Backbone.Collection.extend({
-        model: rTodoModel,
-        url: '/api2/getTodo.php',
-    });
-    return Collection;
-});
