c60d7ac16e42dcb0c466e73e8a01db6752a59a83
kvark85 <kvark1985@gmail.com>
2016-03-27 21:35:21 +0300

    Добавлены обработчики на кнопку "Сохранить" и на кнопку Enter при вводе заметки.

--- js/app/todo/todoContentView.js
+++ js/app/todo/todoContentView.js
@@ -10,8 +10,14 @@ define(['backbone',
             el: '#content',
             template: _.template(template),
 
+            events: {
+                'keypress #newTodo': 'createNewTodoOnEnter',
+                'click #butAddTodo': 'addNewTodoToList'
+            },
+
             initialize: function () {
                 this.render();  //рендер контентной части
+                this.$newTodo = this.$el.find('#newTodo');
             },
 
             render: function () {
@@ -21,6 +27,19 @@ define(['backbone',
                     var oneTodoView = new OneTodoView({model: todo});
                     this.$('#todoList').append(oneTodoView.render().el);
                 }, this);
+            },
+
+            createNewTodoOnEnter: function (e) {
+                if (e.keyCode !== 13 || !this.$newTodo.val()) {
+                    return;
+                }
+
+                this.addNewTodoToList();
+            },
+
+            addNewTodoToList: function () {
+                var stringTodo = this.$newTodo.val().trim();
+                this.collection.create({title: stringTodo});
             }
         });
         return View;
