c7e6723f2c17193660ccdcc05121c3ebe2adaf0c
kvark85 <kvark1985@gmail.com>
2016-07-04 23:37:58 +0300

    Добавил build.js файл.

---
+++ build.js
@@ -0,0 +1,27 @@
+({
+    dir: 'dist',
+    modules: [
+        {
+            name: 'app'
+        }
+    ],
+    fileExclusionRegExp: /^(r|build)\.js$/,
+    optimizeCss: 'standard',
+    removeCombined: true,
+    paths: {
+        jquery: 'js/lib/jquery/dist/jquery.min',
+        text: 'js/lib/text/text',
+        underscore: 'js/lib/underscore/underscore-min',
+        backbone: 'js/lib/backbone/backbone-min',
+        bootstrap: 'js/lib/bootstrap/dist/js/bootstrap.min'
+    },
+    shim: {
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        }
+    }
+})
