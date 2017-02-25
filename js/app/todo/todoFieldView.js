2ae2a489e8a85491e7333d5d935c4122946939c3
andrey_bespaliy <andrey_bespaliy@csltd.com.ua>
2017-02-25 18:35:37 +0200

    Промежуточный коммит для разработчика.

--- js/app/todo/todoFieldView.js
+++ js/app/todo/todoFieldView.js
@@ -1,44 +1,43 @@
 define(['backbone',
-        'text!templates/todo/todoFieldTemplate.html',
         'app/todo/header/todoHeaderView',
         'app/todo/todoContentView',
         'app/todo/header/todoUserModel',
         'app/todo/todoListCollection'],
     function (Backbone,
-             template,
              TodoHeaderView,
              TodoContentView,
              TodoUserModel,
              TodoListCollection) {
         'use strict';
         var View = Backbone.View.extend({
-            el: '#mainContetn',
-            template: _.template(template),
+            el: '#content',
 
             initialize: function () {
-                this.todoUserModel = new TodoUserModel();
-                this.todoListCollection = new TodoListCollection();
-                this.render();
-                this.todoUserModel.fetch({
-                    success: function () {
-                        this.header.render();
-                    }.bind(this),
-                    error: function () {
-                        location.href = '';
-                    }
-                });
-                this.todoListCollection.fetch({
-                    error: function () {
-                        location.href = '';
-                    }
-                });
+                alert('init todo field');
+                this.todoUserModel = new TodoUserModel(); // create Users object (model)
+                this.todoListCollection = new TodoListCollection(); // create collections object (collection)
+                //this.render();
+                //this.todoUserModel.fetch({
+                //    success: function () {
+                //        this.header.render();
+                //    }.bind(this),
+                //    error: function () {
+                //        location.href = '';
+                //    }
+                //});
+                //this.todoListCollection.fetch({
+                //    error: function () {
+                //        location.href = '';
+                //    }
+                //});
             },
 
             render: function () {
-                this.$el.html(this.template());
-                this.header = new TodoHeaderView({model: this.todoUserModel});
-                this.content = new TodoContentView({collection: this.todoListCollection});
-                return this;
+                alert('field render');
+                //this.$el.html(this.template());
+                //this.header = new TodoHeaderView({model: this.todoUserModel});
+                //this.content = new TodoContentView({collection: this.todoListCollection});
+                //return this;
             }
         });
         return View;
